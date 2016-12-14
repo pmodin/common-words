9174c07b0b78fd8093e50e18f15befb958f87109
Per Modin <per@modin.io>
2016-12-14 14:35:30 +0100

    Add filter for categories
    Useful to filter robot articles

--- common_words.rb
+++ common_words.rb
@@ -1,11 +1,35 @@
-# Build a list of common words, ignoring case, using Wikipedia as source.
-# Configure language and number of queries to your liking. Will print current
-# title as progress (to STDERR).
-#
-# Suggested usage:
-#   ruby common_words.rb > outfile.txt
-
 require 'wikipedia'
+require 'optparse'
+
+options = {
+  times: 10
+}
+
+def skip?(categories, rejections)
+  categories.product(rejections).detect do |cat, reject|
+    cat.match(reject)
+  end
+end
+
+OptionParser.new do |opts|
+  opts.banner = <<-EOF
+Build a list of common words, ignoring case, using Wikipedia as source.
+Configure language and number of queries to your liking. Will print current
+title as progress (to STDERR).
+
+Suggested usage:
+  ruby common_words.rb [-n 20] > outfile.txt
+
+EOF
+
+  opts.on('-n', '--times N', 'Amount of times') do |v|
+    options[:times] = Integer(v)
+  end
+
+  opts.on('-x', '--reject CAT[,CAT[,CAT]...]', 'Reject these categories') do |v|
+    options[:reject] = v.split(',')
+  end
+end.parse!
 
 Wikipedia.Configure do
   domain 'sv.wikipedia.org'
@@ -13,9 +37,10 @@ Wikipedia.Configure do
 end
 
 words = {}
-
-100.times do |i|
+options[:times].times do |i|
   article = Wikipedia.find_random
+  redo if options[:reject] && skip?(article.categories, options[:reject])
+
   STDERR.puts "#{i}: #{article.title}"
   article.text.split.each do |x|
     y = x.downcase[/[[:alpha:]]*/]
@@ -24,4 +49,3 @@ words = {}
 end
 
 (words.sort_by { |_word, count| count }).reverse.each { |x| puts x.first }
-
